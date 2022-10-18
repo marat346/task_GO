77f964b2caa0703c6afe87a17c3f76251453001c
marat346 <89637847+marat346@users.noreply.github.com>
2022-10-18 14:42:14 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Привет!Введите любое целое число число")
+
+var a int
+
+fmt.Scan(&a)
+  sum := a * a
+  fmt.Println("Квадрат числа а:",sum)
+}
