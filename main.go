92e565c75311b4d538b8372e12d9014d46dad674
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-04 05:56:37 +0000

    a % 2

--- main.go
+++ main.go
@@ -3,20 +3,14 @@ package main
 import "fmt"
 
 func main() {
+
   var a int
-  var b int 
   
- fmt.Println("Введиет число А :")
- fmt.Scan(&a)
-
-fmt.Println("Введите второе число B :")
-fmt.Scan(&b)
-
-if a < b {
-    fmt.Println("Число А меньше числа B")
-  } else if a == b {
-  fmt.Println("Число" , b ,"равно числу A")
-}  else {
-    fmt.Println("Число B меньше числа A")
+  fmt.Println("Введите число :")
+  fmt.Scan(&a)
+  if a % 2 == 0 {
+    fmt.Println("Число четное!!!")
+  } else {
+    fmt.Println("Нечетное число")
   }
-}
+  }
