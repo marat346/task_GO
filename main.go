8af218a3a393fa20d2010d54d7da54c5d64a3600
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-09 09:52:00 +0000

    исправил

--- main.go
+++ main.go
@@ -16,18 +16,38 @@ func main(){
   fmt.Println("Введите сумму чека:")
   fmt.Scan(&sum)
  
-  if day == 1 {
-     if guest > 5 {
-      discount:= sum /100 *10
-      bonus := sum / 100 * 10
-      sum = sum - discount + bonus 
-      fmt.Println("Скидка по понедельникам:",discount,"рублей")
-      fmt.Println("Надбавка на обслуживание:", bonus)
-      fmt.Println("Сумма к оплате:", sum,"рублей")
-    } else if {
-      discount:= sum /100 *10
-       sum -= discount
-       fmt.Println("Скидка по понедельникам:",discount,"рублей")
-       fmt.Println("Сумма к оплате:", sum,"рублей")
-      
-}
+        if day == 1 {
+          if guest > 5 {
+         discount:= sum /100 *10
+         bonus := sum / 100 * 10
+         sum = sum - discount + bonus 
+         fmt.Println("Скидка по понедельникам:",discount,"рублей")
+         fmt.Println("Надбавка на обслуживание:", bonus)
+         fmt.Println("Сумма к оплате:", sum,"рублей")
+         } else {
+            discount:= sum /100 *10
+            sum -= discount
+            fmt.Println("Скидка по понедельникам:",discount,"рублей")
+            fmt.Println("Сумма к оплате:", sum,"рублей")
+            }
+          } else if sum > 10000  {
+              if day == 5 {
+                if guest > 5 {
+               discount:= sum /100 * 5
+               bonus := sum / 100 * 10
+               sum = sum - discount + bonus
+               fmt.Println("Скидка по понедельникам:",discount,"рублей")
+               fmt.Println("Надбавка на обслуживание:", bonus)
+               fmt.Println("Сумма к оплате:", sum,"рублей")
+          }
+                } 
+          } else {
+          if guest > 5 {
+            bonus := sum / 100 * 10
+            sum+= bonus
+            fmt.Println("Сумма к оплате:", sum,"рублей")
+          } else {
+            fmt.Println("Сумма к оплате:", sum,"рублей")
+          }
+          }
+  }
---
+++ practice1/restoran.go
@@ -0,0 +1,53 @@
+ package main
+
+import "fmt"
+
+func main(){
+  var day int
+  var guest int
+  var sum int
+  
+  fmt.Println("Введите день недели:")
+  fmt.Scan(&day)
+
+  fmt.Println("Введите число гостей:")
+  fmt.Scan(&guest)
+
+  fmt.Println("Введите сумму чека:")
+  fmt.Scan(&sum)
+ 
+        if day == 1 {
+          if guest > 5 {
+         discount:= sum /100 *10
+         bonus := sum / 100 * 10
+         sum = sum - discount + bonus 
+         fmt.Println("Скидка по понедельникам:",discount,"рублей")
+         fmt.Println("Надбавка на обслуживание:", bonus)
+         fmt.Println("Сумма к оплате:", sum,"рублей")
+         } else {
+            discount:= sum /100 *10
+            sum -= discount
+            fmt.Println("Скидка по понедельникам:",discount,"рублей")
+            fmt.Println("Сумма к оплате:", sum,"рублей")
+            }
+          } else if day == 5  {
+              if sum > 10000 {
+                if guest > 5 {
+               discount:= sum /100 * 5
+               bonus := sum / 100 * 10
+               sum = sum - discount + bonus
+               fmt.Println("Скидка по понедельникам:",discount,"рублей")
+               fmt.Println("Надбавка на обслуживание:", bonus)
+               fmt.Println("Сумма к оплате:", sum,"рублей")
+           }
+                }
+          } else {
+          if guest > 5{
+            bonus := sum / 100 * 10
+            sum+= bonus
+            fmt.Println("Сумма к оплате:", sum,"рублей")
+          } else {
+            fmt.Println("Сумма к оплате:", sum,"рублей")
+          }
+          }
+  }
