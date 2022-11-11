ceda3aed1a05e2ec000932422b30af693a745f58
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-11 14:47:27 +0000

    edit

--- practice_2/equation.go
+++ practice_2/equation.go
@@ -1,8 +1,11 @@
  package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main(){
+func main() {
   var a float64
   var b float64
   var c float64
@@ -10,12 +13,27 @@ func main(){
   fmt.Println("Введите коэффициент А квадратного уравнения :")
   fmt.Scan(&a)
 
-  fmt.Println("Введите коэффициент А квадратного уравнения :")
+  fmt.Println("Введите коэффициент B квадратного уравнения :")
   fmt.Scan(&b)
   
-  fmt.Println("Введите коэффициент А квадратного уравнения :")
+  fmt.Println("Введите коэффициент C квадратного уравнения :")
   fmt.Scan(&c)
-
-  pow(b ,b)
-  fmt.Println(b)
+  
+  f := math.Pow(b, 2)
+  diskriminant := f -  4 * a * c
+	fmt.Println(diskriminant)
+  
+  if diskriminant < 0 {
+    fmt.Println("У уравнения нету корней,так как дискриминант отрицательный :",diskriminant)
   }
+  if diskriminant > 0 {
+    - b = b
+    fmt.Println( b)
+    d := (-b + math.Sqrt(f)) / (2 * a)
+    fmt.Printf("%.1f",d)
+  }
+  if diskriminant == 0 {
+    z := - b / 2 * a
+    fmt.Printf("%.1f",z)
+  }
+}
