22f8443656eb028cd30efd986df696833f9dd0e3
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-14 09:02:24 +0000

    edit2

--- main.go
+++ main.go
@@ -4,20 +4,29 @@ import "fmt"
  
 func main() {
   
-  var discount float64
-  var sum float64
+  one := 0
+  second := 0
+  third := 0 
 
-  fmt.Println("Введите сумму товара :")
-  fmt.Scan(&sum)
-
-  fmt.Println("Введите скидку :")
-  fmt.Scan(&discount)
-
-  maxDiscount := sum * 0.3
-  
-  if maxDiscount >= discount && discount <= 2000 {
-    fmt.Println("Вам положена скидка в размере" , discount, "рублей")
-  } else {
-    fmt.Println("Вам не положена скидка")
+  for i := 0; i <= 1000; i++{
+    fmt.Println(i)
+    one+= i
+    if one % 10 != 0 {
+      one++
+      continue
+     }
+    fmt.Println("Наполнена one на 10")
+    
+    if second % 100 != 0 {
+       second++
+         continue
+     }
+    fmt.Println("Наполнена one на 100")
+    
+    if third % 1000 != 0 {
+      third++
+      continue
+    }
+    fmt.Println("Наполнена one на 1000")
   }
 }
