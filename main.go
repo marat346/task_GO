033763224fd68d2187da2c530a5bf5b28a8144a0
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-15 09:23:02 +0000

    edit

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import fmt 
-time
+import "fmt"
  
 func main() {
-   fmt.Println(time.Now())
-}
+ a:= 0
+  for numbers:= 0; numbers <= 999999;numbers++{
+    leftSide := numbers / 10000
+     oneLeftSide := (numbers % 1000) / 100
+     rightSide := (numbers % 100) / 10
+     secondRightSide := numbers % 10
+    
+   if leftSide == secondRightSide && rightSide == oneLeftSide{
+    a++
+     fmt.Println("Билетик Зеркальный")
+     fmt.Println(a)
+  }
+    
+ }
+   }
