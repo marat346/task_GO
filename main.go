46504c17b08773b4d5a5c801a47cccdd49211e25
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-15 14:54:43 +0000

    edit

--- main.go
+++ main.go
@@ -4,20 +4,19 @@ import "fmt"
  
 func main() {
    a :=0
-   for numbers:= 0; numbers <= 999999;numbers++{
+   for numbers:= 100000; numbers <= 999999;numbers++{
+   numbers1 := numbers / 100000
+   numbers2 := numbers % 100000 / 10000
+   numbers3 := numbers % 10000 / 1000
+   numbers4 := numbers % 1000 / 100
+   numbers5 := numbers % 100 / 10
+   numbers6 := numbers % 10
    
-  numbers1 := numbers / 100000
-  numbers2 := numbers % 100000 / 10000
-  numbers3 := numbers % 10000 / 1000
-  numbers4 := numbers % 1000 / 100
-  numbers5 := numbers % 100 / 10
-  numbers6 := numbers % 10
-   
-  if numbers1 == numbers6 && numbers2 == numbers5 && numbers3 == numbers4 {
-
-      fmt.Println("Билетик Зеркальный")
-      a++
-    }
-     fmt.Println(a)
-   }
+   if numbers1 == numbers6 && 
+       numbers2 == numbers5 && 
+        numbers3 == numbers4 {
+          a++
+  }
+}
+ fmt.Println(" Количество зеркальных билетов среди всех шестизначных номеров от 100000 до 999999 :" , a) 
 }
