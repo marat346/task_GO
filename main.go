770645d59e52ba072fd75531e0851f47a539ed42
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-15 08:56:14 +0000

    edit

--- main.go
+++ main.go
@@ -1,32 +1,8 @@
 package main
 
-import "fmt"
+import fmt 
+time
  
 func main() {
-   
-  one := 0
-  second := 0
-  third := 0
-  
-  for i := 0; i <= 1000; i ++ {
-   fmt.Println(i)
-    
-  one ++
-  if one == 10 {
-      fmt.Println("Переменная ONE заполнилась на числе 10")
-      continue
-     } 
-    
-  second++
-  if second == 100 {
-      fmt.Println("Переменная Second заполнилась на 100")
-      continue
-    }
-
-  third++ 
-  if third == 999 {
-    fmt.Println("Переменная Third заполнилась на 1000")
-      continue
-   } 
-  }
+   fmt.Println(time.Now())
 }
