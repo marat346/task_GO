1f46103cdf571d91d3cee1cad42895a2b08e9dd8
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-15 14:40:28 +0000

    edit

--- main.go
+++ main.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
  
 func main() {
- a:= 0
-  for numbers:= 0; numbers <= 999999;numbers++{
-    leftSide := numbers / 10000
-     oneLeftSide := (numbers % 1000) / 100
-     rightSide := (numbers % 100) / 10
-     secondRightSide := numbers % 10
-    
-   if leftSide == secondRightSide && rightSide == oneLeftSide{
-    a++
-     fmt.Println("Билетик Зеркальный")
+   a :=0
+   for numbers:= 0; numbers <= 999999;numbers++{
+   
+  numbers1 := numbers / 100000
+  numbers2 := numbers % 100000 / 10000
+  numbers3 := numbers % 10000 / 1000
+  numbers4 := numbers % 1000 / 100
+  numbers5 := numbers % 100 / 10
+  numbers6 := numbers % 10
+   
+  if numbers1 == numbers6 && numbers2 == numbers5 && numbers3 == numbers4 {
+
+      fmt.Println("Билетик Зеркальный")
+      a++
+    }
      fmt.Println(a)
-  }
-    
- }
    }
+}
