144468ffcb7dd0971c7879b47c17c3f7f5fcce8b
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-18 08:53:59 +0000

    edit

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math"
  
 func main() {
-  var max int8 = -127
-  fmt.Println(max)
-  max = max - 2
-  fmt.Println(max)
+  
+  var i uint8 = 10
+  
+  for i = 0 ; i != 254; i -- {
+    fmt.Println(i)
+  if i == 0 {
+    fmt.Println("кола закончилась")
+    continue
+  }
+  fmt.Printf("Осталось бутылок %d \n" , i)
 }
+  }
