1ec2756122e36de661dc07317511633d0058fecc
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-18 18:14:35 +0000

    edit

--- main.go
+++ main.go
@@ -5,15 +5,28 @@ import (
   "math"
  )
 func main() {
-  
-for i := 0; i <= math.MaxUint32;i++ {
-     if i == math.MaxUint8 {
-    fmt.Println("Приходится переполнений чисел типа uint8 :",i)
-    continue
+  var a int16
+  var b int16
+
+  fmt.Println("Минимальный тип данных.")
+  fmt.Println("_______________________")
+  fmt.Println("Введите первое число А :")
+  fmt.Scan(&a)
+
+  fmt.Println("Введите второе число В :")
+  fmt.Scan(&b)
+  rsl := int32(a) * int32(b)
+  fmt.Println(rsl)
+
+  if rsl <  math.MaxUint8 && rsl > 0 {
+      fmt.Println("Лучше сохранить в тип Uint8")
+    } else if rsl <  math.MaxUint16 && rsl > 0 {
+       fmt.Println("Лучше сохранить в тип int16")
+    } else if rsl < math.MaxInt32 && rsl < 0 {
+    fmt.Println("Лучше сохранить в тип int32")
+   } else if rsl > 0 {
+    fmt.Println("Лучше сохранить в тип Uint32")
+   } else {
+    fmt.Println("Лучше сохранить в тип int8")
+   }
   }
-     if i == math.MaxUint16 {
-    fmt.Println("Приходится переполнений чисел типа uint16 :",i)
-    continue
-  }
- }
-}
