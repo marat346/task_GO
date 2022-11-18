fd92e25beb80a241f13bebe14e3aa0ea4db7982e
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-18 06:28:50 +0000

    edit

--- main.go
+++ main.go
@@ -3,30 +3,8 @@ package main
 import "fmt"
  
 func main() {
-  
-  fmt.Println("Введите будний день недели:")
-  
-  var day string
-  
-  fmt.Scan(&day )
-
-  switch day {
-    
-    case  "пн":
-    fmt.Println("понедельник")
-    fallthrough
-    case  "вт":
-    fmt.Println("вторник")
-    fallthrough
-    case  "ср":
-    fmt.Println("среда")
-    fallthrough
-    case  "чт":
-    fmt.Println("четверг")
-    fallthrough
-    case  "пт":
-    fmt.Println("пятница")
-    default:
-    fmt.Println("Вы ввели неправильно")
-    }
+  var max int8 = -127
+  fmt.Println(max)
+  max = max - 2
+  fmt.Println(max)
 }
