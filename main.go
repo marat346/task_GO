1e873d1b8355f2a435cf2b3c9bce974656e7cf85
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-21 08:27:18 +0000

    edit

--- main.go
+++ main.go
@@ -5,5 +5,14 @@ import (
   "math"
  )
 func main() {
+  var a float64
+  a = 0
+  epsilon := 0.0001
+  for i :=0; i <15;i++{
+    a+= 0.1
+    if math.Abs(a - 1) < epsilon{
+      fmt.Println("a приблизительно равно 1")
+    }
+  }
   
   }
