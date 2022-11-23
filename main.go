611b8c51b5f25758f01c9f0b76c097bccc9d73c9
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-23 07:26:46 +0000

    edit

--- main.go
+++ main.go
@@ -5,32 +5,15 @@ import (
   "math"
  )
 func main() {
-  var a int16
-  var b int16
-
-  fmt.Println("Минимальный тип данных.")
-  fmt.Println("_______________________")
-  fmt.Println("Введите первое число А :")
-  fmt.Scan(&a)
-
-  fmt.Println("Введите второе число В :")
-  fmt.Scan(&b)
-  rsl := int32(a) * int32(b)
-  fmt.Println(rsl)
-
-  if rsl > 0 {
-    if rsl <= math.MaxUint8 {
-      fmt.Println("Попробуйте лучше тип данных Uint8")
-    } else if rsl <= math.MaxUint16 {
-      fmt.Println("Попробуйте лучше тип данных Uint16")
-    } else {
-      fmt.Println("Попробуйте лучше тип данных Uint32")
+  var a float64
+  a = 0
+  epsilon := 0.0001
+  for i := 0;i < 15;i++{
+    a+= 0.1
+    fmt.Println(a)
+    if math.Abs(a - 1) < epsilon{
+      fmt.Println(a)
+      fmt.Println("a приблизительно равно 1")
     }
-  } else if rsl >= math.MinInt8 {
-    fmt.Println("Попробуйте лучше тип данных Int8")
-  } else if rsl >= math.MinInt16 {
-    fmt.Println("Попробуйте лучше тип данных Int16")
-  } else {
-    fmt.Println("Попробуйте лучше тип данных Int32")
   }
-  }
+}
