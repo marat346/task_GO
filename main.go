4a78b3b0c2abf0fc53b1bd5d9a39e91cfeea350c
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-23 08:03:52 +0000

    edit

--- main.go
+++ main.go
@@ -5,15 +5,29 @@ import (
   "math"
  )
 func main() {
-  var a float64
-  a = 0
-  epsilon := 0.0001
-  for i := 0;i < 15;i++{
-    a+= 0.1
-    fmt.Println(a)
-    if math.Abs(a - 1) < epsilon{
-      fmt.Println(a)
-      fmt.Println("a приблизительно равно 1")
+ var epsilon float64
+ epsilon = 0.0001
+  
+  fmt.Println("Введите х для которого необходимо рассчитать sin ")
+  
+  var x float64
+  fmt.Scan(&x)
+  
+  result := 0.0
+  prevResult := 0.0
+  fact := 1
+  k := 0 
+  
+  for {
+    if k > 0 {
+      fact *= 2 * k * (2*k + 1) 
     }
+    result += math.Pow(-1,float64(k)) * math.Pow(x,float64(2 * k + 1)) / float64(fact)
+    if math.Abs(result - prevResult ) < epsilon {
+      fmt.Println(result)
+      break
+    }
+    k++
+    prevResult = result
   }
 }
