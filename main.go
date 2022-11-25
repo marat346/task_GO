e526c150ec7872d262c36271a1c0a91ec7374c4d
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-25 18:29:30 +0000

    edit

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
   "fmt"
   "math"
  )
+
 func main() {
-  var cycle_uint8 int
-  var cycle_uint16
-for i := 0; i <= math.MaxUint32;i++ {
-     if i == math.MaxUint8 {
-       cycle_uint8 += 1
-    }
- }
+  var cycle_uint8 = 0
+  var cycle_uint16 = 0
+  
+for i:= 0; i < math.MaxUint32; i++ {
+  if i % math.MaxUint8 == 0 {
+       cycle_uint8++
+        continue
+     }
+  if i % math.MaxUint16 == 0 {
+       cycle_uint16++
+        continue
+     }
+  }
+  fmt.Println(cycle_uint8)
+  fmt.Println(cycle_uint16)
 }
