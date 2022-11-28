f1e8f113e1908785a0f6766280cd99e2115e0cb9
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-28 07:13:30 +0000

    edit

--- practice_9.7_(6)/uint8_uint16.go
+++ practice_9.7_(6)/uint8_uint16.go
@@ -4,10 +4,19 @@ import (
   "fmt"
   "math"
  )
+
 func main() {
+  var cycle_uint8 = 0
+  var cycle_uint16 = 0
   
-for i:= 0; i <= math.MaxUint32; i++ {
-     
+for i:= 0; i < math.MaxUint32; i++ {
+  if i % math.MaxUint16 == 0 {
+       cycle_uint16++
+     }
+  if i % math.MaxUint8 == 0 {
+       cycle_uint8++
+     }
   }
- }
+  fmt.Println("Приходится",cycle_uint8," переполнений чисел типа uint8")
+  fmt.Println("Приходится",cycle_uint16," переполнений чисел типа uint16")
 }
