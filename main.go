2202ceb61993e72c861909b9f9fdac31ac562975
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-29 05:13:10 +0000

    edit

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
+  "strings"
 )
 
 func main() {
-  s:= "hello wordl"
-
-  for strings.Contains(s," "){
-    spaceIndex:= strings.Index(s," ")
-    fmt.Println(s[:spaceIndex])
-    s = s[spaceIndex + 1:]
-  }
- fmt.Println(s) 
-}
+ s:= "hello  wordl "
+s = [0:5] + [6:12]
+  fmt.Println(a)
+} 
