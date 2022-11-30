eda119ca4b49522b7bbbc873fe5a730fcf02ba09
marat346 <89637847+marat346@users.noreply.github.com>
2022-11-30 05:48:07 +0000

    edit

--- main.go
+++ main.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"fmt"
-  "strings"
+	"strconv"
+	"strings"
 )
 
 func main() {
-  fmt.Println("Определение количества слов, начинающихся с большой буквы в строке:")
-  
-  
-  line_1 := " Go is an Open source programming Language that makes it Easy to build simple, reliable, and efficient Software."
-  line_2 := strings.ToLower(line_1)
-  count:= 0
-
-  for len(line_1) > 0 {
-    if line_1[:1] != line_2[:1] {
-      count++ 
-    }
-    line_1 = line_1[1:]
-    line_2 = line_2[1:]
+  line := "10 "
+  line = strings.Trim(line)
+  i,err :=strconv.Atoi(line)
+  if err != nil{
+    fmt.Println("ошибка")
+    return
   }
-  fmt.Println("Строка содержит", count ,"слов с большой буквы.")
+fmt.Println(i)
+  
   }
