4809e9a5228533b54a663b878c60e2ab05c29fa4
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-03 06:45:32 +0000

    edit

--- practice_11.6_(8)/count_word.go
+++ practice_11.6_(8)/count_word.go
@@ -12,13 +12,7 @@ func main() {
   line_1 := " Go is an Open source programming Language that makes it Easy to build simple, reliable, and efficient Software."
   line_2 := strings.ToLower(line_1)
   count:= 0
-
-  for len(line_1) > 0 {
-    if line_1[:1] != line_2[:1] {
-      count++ 
-    }
-    line_1 = line_1[1:]
-    line_2 = line_2[1:]
-  }
+for len > line_1
+  
   fmt.Println("Строка содержит", count ,"слов с большой буквы.")
   }
