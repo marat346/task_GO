7d01c03640e3afe79fcd32e197c38455ad9bc37f
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-03 10:56:30 +0000

    edit

--- main.go
+++ main.go
@@ -9,16 +9,20 @@ func main() {
   fmt.Println("Определение количества слов, начинающихся с большой буквы в строке:")
   
   
-  line_1 := " Go is an Open source programming Language that makes it Easy to build simple, reliable, and efficient Software."
+  line_1 := "Go is an Open source programming Language that makes it Easy to build simple, reliable, and efficient Software."
 count:= 0
   
 for len(line_1) > 0 {
-    index := strings.Index(line_1," ")
-    word := line_1[:index]
-   if word == strings.Title(word) {
+     index := strings.Index(line_1," ")
+     word := line_1[:index]
+  if word == strings.Title(word) {
+      count++
+    }
+    line_1 = line_1 [index + 1:]
+  if line_1 == strings.Title(line_1) {
     count++
+    break
     }
   }
-
   fmt.Println("Строка содержит", count ,"слов с большой буквы.")
   }
