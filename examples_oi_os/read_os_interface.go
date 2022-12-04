d5d48c3d5484629246ad22edf68568b4773c8cba
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-04 07:41:59 +0000

    edit

---
+++ examples_oi_os/read_os_interface.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+  "fmt"
+  )
+
+func main() {
+  f,err := os.Open("log.txt")
+  if err != nil{
+    fmt.Println("Ошибка открытия файла", err)
+    return
+  }
+  defer f.Close()
+  buf := make ([]byte ,150)
+  _,err = f.Read(buf)
+  if err != nil{
+    fmt.Println("Не смогли прочитать последовательность байтов из файла", err)
+    return
+  }
+  fmt.Println(string(buf))
+}
