7ab8e5844ee915dbedf1b7588ee39f638de6b50b
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-06 16:54:40 +0000

    edit

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"fmt"
   "time"
+  "os"
 	)
 
 func main() {
-  now := time.Now()
-  fmt.Println(now.Format("2006-01-02 15:04:05"))
-}
+  var line string
+  fmt.Println("Введите строку:")
+  fmt.Scan(&line)
+
+  file,err := os.Create("sale_garage.txt")
+  if err != nil{
+    fmt.Println("Файл не удаеться открыть")
+    return
+  }
+  defer file.Close()
   
+  file.WriteString(time.Now().Format("2006-01-02 15:04:05"))
+  file.WriteString(fmt.Sprintf(line))
+}
---
+++ time_date.go/data.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+  "time"
+	)
+
+func main() {
+  now := time.Now()
+  fmt.Println(now.Format("2006-01-02 15:04:05"))
+}
+  
