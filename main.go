7df14692991cf84c70e0858dff34f4cc46a7b911
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-07 09:16:26 +0000

    edit

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+ package main
 
 import (
 	"fmt"
@@ -8,8 +8,14 @@ import (
 
 func main() {
   var line string
+  var number_line string
+  
   fmt.Println("Введите строку:")
   fmt.Scan(&line)
+  
+  fmt.Println("Введите номер строки:")
+  fmt.Scan(&number_line)
+  
 
   file,err := os.Create("sale_garage.txt")
   if err != nil{
@@ -17,7 +23,10 @@ func main() {
     return
   }
   defer file.Close()
-  
+  file.WriteString("Номер строки:\n")
+  file.WriteString(number_line)
   file.WriteString(time.Now().Format("2006-01-02 15:04:05"))
-  file.WriteString(fmt.Sprintf(line))
+  file.WriteString("\n")
+  file.WriteString(line)
+
 }
