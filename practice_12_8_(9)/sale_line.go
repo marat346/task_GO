f71aa3b30a4112ecf64f0398fcfb4603e3f59d9a
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-07 09:40:44 +0000

    edit

--- practice_12_8_(9)/sale_line.go
+++ practice_12_8_(9)/sale_line.go
@@ -1,23 +1,32 @@
-package main
+ package main
 
 import (
 	"fmt"
+  "time"
   "os"
 	)
 
 func main() {
   var line string
+  var number_line string
+  
   fmt.Println("Введите строку:")
   fmt.Scan(&line)
   
+  fmt.Println("Введите номер строки:")
+  fmt.Scan(&number_line)
+  
+
   file,err := os.Create("sale_garage.txt")
-  if err != nil {
-    fmt.Println("Не смогли создать файл.", err)
+  if err != nil{
+    fmt.Println("Файл не удаеться открыть")
     return
   }
   defer file.Close()
-  
+  file.WriteString("Номер строки:\n")
+  file.WriteString(number_line)
+  file.WriteString(time.Now().Format("2006-01-02 15:04:05"))
+  file.WriteString("\n")
   file.WriteString(line)
-  fmt.Println(file.Name())
+
 }
-  
