45f89be8a494c60e0ff2e85df0ac837768331e0f
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-09 16:01:59 +0000

    edit

--- main.go
+++ main.go
@@ -1,23 +1,47 @@
- package main
+package main
 
 import (
+	"bufio"
 	"fmt"
-  "os"
-	)
+	"os"
+	"time"
+  "io/ioutil"
+  "bytes"
+)
 
 func main() {
-
-  text := "access denial"
+  var exit string = "exit"
+  var number_line int = 0
+  var b bytes.Buffer
   
-  f,err := os.Create("access.txt")
-  if err := os.Chmod("access.txt",0444); err != nil {
-    fmt.Println(err)
+  scanner := bufio.NewScanner(os.Stdin)
+  fmt.Println("Введите строку:")
+  
+  for scanner.Scan() {
+  
+  number_line ++
+  if scanner.Text() == exit {
+    fmt.Println("Выход из  программы")
+    break
   }
+   b.WriteString(fmt.Sprintf("%v %s %v \n", number_line, time.Now().Format("2006-01-02 15:04:05"), scanner.Text()))
+  }
+  
+  fileName := "sale_garage.txt"
+  if err := ioutil.WriteFile(fileName,b.Bytes(),0666);err != nil {
+    panic(err)
+  }
+    file, err := os.Open(fileName)
   if err != nil{
-    fmt.Println(err)
+    panic(err)
   }
-  defer f.Close()
-
-  f.WriteString(text)
+  defer file.Close()
   
+  resultBytes , err := ioutil.ReadAll(file)
+  if err != nil{
+    panic(err)
+  }
+  fmt.Println("Сохраненный лог:")
+  fmt.Println(string(resultBytes))
+
 }
