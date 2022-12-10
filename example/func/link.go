eee473301d19de81a56578005f9ba28fc1edab59
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-10 15:46:52 +0000

    edit

--- example/func/link.go
+++ example/func/link.go
@@ -1,1 +1,13 @@
-package 
+package main
+
+import "fmt"
+
+func makeItZero(x int){
+  x = 0
+}
+
+func main() {
+  a:= 0
+  makeItZero(a)
+  fmt.Println(a)
+}
--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import "fmt"
 
-func makeItZero(x int){
-  x = 0
-}
+func sum(a,b int){
+  if a > b {
+    c:= b
+    b = a
+    a = c
+    }
+  s:=0
+  for i := a;i <= b;i++ {
+   if i % 2 == 0 {
+       s = s + i
+      }
+    } 
+  fmt.Println(s)
+  }
 
 func main() {
-  a:= 0
-  makeItZero(a)
-  fmt.Println(a)
+  sum(8,12)
+  sum(12,8)
+  sum(7,12)
 }
