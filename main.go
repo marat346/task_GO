3c033613afaff80b5cc415e0829edcd36ba66d46
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-15 17:51:48 +0000

    edit

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 )
-func last (arrayInput  [10] int) (arrayOutput [10] int) {
-  for i := 0;i < arrayInput[len(arrayInput) -1];i++ {
-    arrayInput[i] = arrayInput[len(arrayInput) -1]
+  
+  func arrayRevers (input [10] int ) (output [10] int) {
+    for i := 0;i < output [len(output) -1] ;i++ {
+    output[i] = output[len(output) - 1] - i
+      fmt.Println(output[i])
+     }
+    return
   }
-}
-
-
-func main() {
- 
-  }
+  
+  func main() {
+    arrayInput := [10] int {1,2,3,4,5,6,7,8,9,10}
+    for _, conclusion := range arrayRevers(arrayInput){
+      fmt.Println(conclusion)
+    }
+    }
