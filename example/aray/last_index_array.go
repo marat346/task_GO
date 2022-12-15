386fa803c42aa0b03e75c897781b020a2e69f74f
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-15 17:09:51 +0000

    edit

---
+++ example/aray/last_index_array.go
@@ -0,0 +1,2 @@
+a := []string{"A", "B", "C"}
+s := a[len(a)-1] // C
--- main.go
+++ main.go
@@ -3,22 +3,9 @@ package main
 import (
 	"fmt"
 )
-var global1 = 1
-var global2 = 2
-var global3 = 3 
-
-func first (a int) int {
-  return  a + global1 + global2
-}
-
-func second (a int) int {
-  return a + global3 + global2
-  }
-
-func third (a int) int {
- return a + global3 +global1
-}
 
 func main() {
-  fmt.Println(first(second(third(10))))
+  a := []string{"A", "B", "C"}
+  s := a[len(a)-1] // C
+  fmt.Println(s)
   }
