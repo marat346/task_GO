744bdbe26f33a47448f58ec121cc28bd477222f3
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-16 15:35:24 +0000

    edit

--- main.go
+++ main.go
@@ -5,16 +5,19 @@ import (
 )
   
   func arrayRevers (input [10] int ) (output [10] int) {
-    for i := 0;i < output [len(output) -1] ;i++ {
-    output[i] = output[len(output) - 1] - i
-      fmt.Println(output[i])
-     }
-    return
+    for i := 0;i < len(input) - 1 ;i++ {
+    output[i] = input [len(input) - 1] - i
+      }
+   return
   }
   
   func main() {
-    arrayInput := [10] int {1,2,3,4,5,6,7,8,9,10}
-    for _, conclusion := range arrayRevers(arrayInput){
-      fmt.Println(conclusion)
+    var numbers [10] int
+    for i:= 0;i < len(numbers) - 1;i++ {
+      fmt.Scan(&numbers[i])
     }
+    for _, end := range arrayRevers(numbers){
+      fmt.Println(end)
+    }
+    
     }
--- practice_15.6(12)/array_one_last.go
+++ practice_15.6(12)/array_one_last.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 )
-func last (arrayInput  [10] int) (arrayOutput [10] int) {
-  for i := 0;i < arrayInput[len(arrayInput) -1];i++ {
-    arrayInput[i] = arrayInput[len(arrayInput) -1]
+  
+  func arrayRevers (input [10] int ) (output [10] int) {
+    for i := 0;i < len(input) - 1 ;i++ {
+    output[i] = input [len(input) - 1] - i
+      }
+   return
   }
-}
-
-
-func main() {
- 
-  }
+  
+  func main() {
+    var numbers [10] int
+    for i:= 0;i < len(numbers) - 1;i++ {
+      fmt.Scan(&numbers[i])
+    }
+    for _, end := range arrayRevers(numbers){
+      fmt.Println(end)
+    }
+    
+    }
