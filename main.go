6195d72f6435d40d79a9e0de9bbe08b4a80572d5
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-19 16:55:32 +0000

    edit

--- main.go
+++ main.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 )
   
-  func arrayRevers (input [10] int ) (output [10] int) {
-    for i := 0;i < len(input) - 1 ;i++ {
-    output[i] = input [len(input) - 1] - i
+func arrayRevers (input [10] int ) (output [10] int) {
+    for i := 0;i < len(output) - 1 ;i++ {
+    output[i] = input [9 - i]
       }
    return
   }
   
-  func main() {
+func main() {
     var numbers [10] int
     for i:= 0;i < len(numbers) - 1;i++ {
-      fmt.Scan(&numbers[i])
+      fmt.Scan(&numbers[i + 1])
     }
     for _, end := range arrayRevers(numbers){
       fmt.Println(end)
