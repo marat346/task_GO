07ae61f68b2f21a830d03a3dbe9ab29a64a89a12
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-20 16:55:02 +0000

    edit

--- practice_19.4_(13)/bubbleSort.go
+++ practice_19.4_(13)/bubbleSort.go
@@ -5,22 +5,18 @@ import (
 )
   
 func main() {
-    
+  
   var array = [10] int {3,1,9,4,7,6,5,8,2,0}
   var min int
-  rsl := 0
   
-  for i:= 0; i < len(array);i++ {
-    
-    for j:= 0; i < len(array) - i;i++{
+  for i:= 0; i < len(array) - 1;i++ {
+    for j:= 0; j < len(array)- 1 - i;j++ {
       if array [j] > array [j + 1] {
-      min = array [j + 1]
-      array [j + 1] = array [j]
-      array [j] = min
-      rsl++
+          min = array [j + 1]
+          array [j + 1] = array [j]
+          array [j] = min
     }
     }
   }
-  fmt.Println(rsl)
   fmt.Println(array)
  }
