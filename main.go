470b7ab9d72415999c304d44be0aa4a0a9b81499
marat346 <89637847+marat346@users.noreply.github.com>
2022-12-23 16:15:07 +0000

    edit

--- main.go
+++ main.go
@@ -3,20 +3,11 @@ package main
 import (
 	"fmt"
 )
-  
+  const matrixRows = 3
+const matrixCols = 3
+
+func determinant 
 func main() {
   
-  var array = [10] int {3,1,9,4,7,6,5,8,2,0}
-  var min int
-  
-  for i:= 0; i < len(array) - 1;i++ {
-    for j:= 0; j < len(array)- 1 - i;j++ {
-      if array [j] > array [j + 1] {
-          min = array [j + 1]
-          array [j + 1] = array [j]
-          array [j] = min
-    }
-    }
-  }
-  fmt.Println(array)
+ 
  }
