e0f391b6763535593925eb895a5b482baf4dbc71
marat346 <89637847+marat346@users.noreply.github.com>
2023-01-04 15:45:20 +0000

    edit

--- main.go
+++ main.go
@@ -1,38 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-const line = 12
-func main() {
-  a:= [line] int {1,2,2,2,3,3,5,6,7,8,9,10}
-  fmt.Println(a)
-  var value int
-  fmt.Println("Введите число:")
-  fmt.Scan(&value)
-  index := find(a,value)
-  fmt.Println("Первое вхождение заданного числа в индексе:", index)
-  }
-  
-func find (a[line] int, value int)(index int){
-  index = 0
-  min := 0
-  max := line - 1
-  for max >= min {
-       middle := (max + min) / 2
-        if a[middle] == value {
-            index = middle
-            break
-        } else if a[middle] > value {
-            max = middle - 1
-        } else {
-            min = middle + 1
-        }
-    }
-  
-    for index > 0 && a[index - 1] == value {
-      index--
-    }
-  return
-  }
+const size = 10
+
+func InsertionSort(array [size]int) [size] int{
+	length := len(array)
+
+	for i := 1; i < length; i++ {
+		j := i
+		   for j > 0 {
+			     if array[j-1] > array[j] {
+				    array[j-1], array[j] = array[j], array[j-1]
+			    }
+			j = j - 1
+		}
+	}
+  return array
+}
+
+func main(){
+  b := [size] int {5,4,3,2,1,10,9,8,7,6}
+  fmt.Println("Исходный массив :\n",b)
+  fmt.Println("Cортировка вставками :\n",InsertionSort(b))
+}
---
+++ practice_23.6_(17)/insertSort.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+const size = 10
+
+func InsertionSort(array [size]int) [size] int{
+	length := len(array)
+
+	for i := 1; i < length; i++ {
+		j := i
+		   for j > 0 {
+			     if array[j-1] > array[j] {
+				    array[j-1], array[j] = array[j], array[j-1]
+			    }
+			j = j - 1
+		}
+	}
+  return array
+}
+
+func main(){
+  b := [size] int {5,4,3,2,1,10,9,8,7,6}
+  fmt.Println("Исходный массив :\n",b)
+  fmt.Println("Cортировка вставками :\n",InsertionSort(b))
+}
