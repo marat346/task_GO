77ede8002dfb78333d3ed5f44889b885d9163760
marat346 <89637847+marat346@users.noreply.github.com>
2023-01-06 10:33:14 +0000

    edit

--- main.go
+++ main.go
@@ -7,25 +7,12 @@ func debugSlice(input[] int){
   fmt.Printf("len:%+v\n",len(input))
   fmt.Printf("cap:%+v\n",cap(input))
 }
-//первый сопоб.Редко используется
-// этот метод нужен когда важен порядок в слайсе
-func remove (slice[]int,index int)[]int {
-  return append(slice[:index],slice[index+1:]...)
-}
-//второй способ.НАИБОЛЕЕ УДОБНЫЙ
-func anotherRemove(slice[]int,index int)[]int {
-  slice[len(slice) - 1],slice[index] =
-  slice[index] ,slice[len(slice) - 1]
-  return slice[:len(slice) - 1]
+
+func anotherRemove(slice[]int)(one[]int,second[]int) {
+  
 }
 
 func main() {
-s := [] int {1,2,3,4,5,6}
-debugSlice(s)
-  // первый способ удаление из  массива
-s= remove(s,2)
-debugSlice(s)
- //второй способ удаление из массива
-s = anotherRemove(s,3)
-debugSlice(s)
+s := [] int {1,2,3,4,5,6,7,8,9,10}
+
 }
