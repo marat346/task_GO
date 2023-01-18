09bffd31b79772299b51385afb84cd930f07fe13
marat346 <89637847+marat346@users.noreply.github.com>
2023-01-18 19:45:41 +0000

    edit

--- main.go
+++ main.go
@@ -8,16 +8,14 @@ import (
 func main() {
 	sentences := [4]string {"Hello world", "Hello Skillbox", "Привет Мир", "Привет Skillbox"}
   chars := [5]rune {'H','E','L','П','М'}
-  
-  fmt.Println(parseTest(sentences,chars))
+  parseTest(sentences,chars)
   }
   
-func parseTest (sentences []string, chars []rune) int {
+func parseTest (sentences []string, chars []rune) {
   for _,v := range sentences {
      index := strings.Index(v," ")
      word := v[index:]
      indexFindWord := strings.IndexRune(word,chars)
-    return indexFindWord
+    
      }
-  }
-   
+  }  
--- practice_24.6_(18)/symbol.go
+++ practice_24.6_(18)/symbol.go
@@ -7,11 +7,15 @@ import (
 
 func main() {
 	sentences := [4]string {"Hello world", "Hello Skillbox", "Привет Мир", "Привет Skillbox"}
-
-for _,v := range sentences {
+  chars := [5]rune {'H','E','L','П','М'}
+  parseTest(sentences,chars)
+  }
+  
+func parseTest (sentences []string, chars []rune) {
+  for _,v := range sentences {
      index := strings.Index(v," ")
      word := v[index:]
-     rRune := strings.IndexRune(word,'H')
-     fmt.Println(rRune)
+     indexFindWord := strings.IndexRune(word,chars)
+    fmt.Println(indexFindWord)
      }
-}	
+  }  
