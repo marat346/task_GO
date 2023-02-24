6819ced1ee41758800f6131895b0b0f612310d06
marat346 <dela346@mail.ru>
2023-02-24 19:37:20 +0500

    edit

---
+++ example/struct/PointerStruct.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// создание структуры с полями
+type Person struct {
+	age  int
+	name string
+}
+
+func main() {
+	p := Person{40, "Marat"}
+	fmt.Println(p)
+	fmt.Println("----------------")
+
+	// создание указателя на структуру &Person
+	pPointer := &p
+	fmt.Println(pPointer)
+	fmt.Println("----------------")
+
+	// создание новой структуры,на прямую указатель на структуру
+	student := &Person{24, "John"}
+	fmt.Println(student)
+	fmt.Println("----------------")
+	//Необязательно через оперсанд "&" иниц.указатель
+	//Можно иницианализировать указатель с помощью встроенный функции NEW
+	pNew := new(Person)
+	fmt.Println(pNew)
+}
