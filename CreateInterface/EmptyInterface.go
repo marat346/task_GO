e9b99f5a84f3a407f5df4cdd0d17997e112f5022
marat346 <dela346@mail.ru>
2023-02-26 13:19:44 +0500

    edit

--- CreateInterface/EmptyInterface.go
+++ CreateInterface/EmptyInterface.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func main() {
+	// передаем различные типы
+	PrintType(1)
+	PrintType("При условии неизвестно какой тип")
+
+	// var empty interface{} = Person{}
+	// empty.???   И как видно на данном интерфейсе нету не каких методов
+}
+
+// ЭТО БУДЕТ ПОЛЕЗНО КОГДА МЫ НЕ ЗНАЕМ КАКОЙ ТИП ПРИЙДЕТ В ФУНКЦИЮ
+func PrintType(i interface{}) {
+	switch i.(type) {
+	case string:
+		fmt.Println("string")
+	case int:
+		fmt.Println("int")
+	}
+}
