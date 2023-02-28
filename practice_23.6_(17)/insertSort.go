639e0a6ce67b377b25a17f2c0e21a2b7960523d0
marat346 <dela346@mail.ru>
2023-02-28 13:16:34 +0500

    edit

--- practice_23.6_(17)/insertSort.go
+++ practice_23.6_(17)/insertSort.go
@@ -2,26 +2,26 @@ package main
 
 import "fmt"
 
-const size = 10
+const s = 10
 
-func InsertionSort(array [size]int) [size] int{
-	
-  length := len(array)
+func InsertionSort(array [s]int) [s]int {
+
+	length := len(array)
 
 	for i := 1; i < length; i++ {
 		j := i
-		   for j > 0 {
-			     if array[j-1] > array[j] {
-				    array[j-1], array[j] = array[j], array[j-1]
-			    }
+		for j > 0 {
+			if array[j-1] > array[j] {
+				array[j-1], array[j] = array[j], array[j-1]
+			}
 			j = j - 1
 		}
 	}
-  return array
+	return array
 }
 
-func main(){
-  b := [size] int {5,4,3,2,1,10,9,8,7,6}
-  fmt.Println("Исходный массив :\n",b)
-  fmt.Println("Cортировка вставками :\n",InsertionSort(b))
+func main() {
+	b := [size]int{5, 4, 3, 2, 1, 10, 9, 8, 7, 6}
+	fmt.Println("Исходный массив :\n", b)
+	fmt.Println("Cортировка вставками :\n", InsertionSort(b))
 }
