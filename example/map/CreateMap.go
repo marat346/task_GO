f26571cccae61e3cdde2ac81a337d019778e12ae
marat346 <dela346@mail.ru>
2023-03-06 10:24:30 +0500

    edit

--- example/map/CreateMap.go
+++ example/map/CreateMap.go
@@ -3,12 +3,19 @@ package main
 import "fmt"
 
 func main() {
+
+	// map ( [ТИП КЛЮЧА] ТИП ЗНАЧЕНИЯ )
 	m := make(map[int]string)
 	fmt.Println(m)
+
 	// добавление в map
 	m[1] = "first"
 	m[2] = "second"
-	fmt.Println(m)
+	fmt.Println(m, "ключ : значение")
 
-	fmt.Println(m[2])
+	fmt.Println(m[2], "обращение к ключу 2 ")
+
+	// удаление из типа map
+	delete(m, 2)
+	fmt.Println(m, "удаление")
 }
--- example/map/LenMap.go
+++ example/map/LenMap.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// сколько ключей записано в карту
 	mLen := map[int]string{
 		1: "first",
 		2: "second",
