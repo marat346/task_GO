1fe571c54097da1f9263e41666d2cd044d114cb9
marat346 <dela346@mail.ru>
2023-03-11 23:24:18 +0500

    edit

--- write/marat.go
+++ write/marat.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+type Marat struct {
+	Name string
+	Age  int
+}
+
+func (a *Marat) PrintInfo(n string) string {
+	return n
+}
+
 func main() {
-	m := make(map[int]string)
-	m[2] = "mama"
-	m[3] = "papa"
-	fmt.Println(m)
+	m := Marat{"Makar", 35}
+
+	fmt.Println(m.PrintInfo("GOGO"))
+
 }
