307cbedd49345285330e6b7f22469f16d8ff56e3
marat346 <dela346@mail.ru>
2023-03-15 12:31:43 +0500

    edit

--- write/marat.go
+++ write/marat.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
+
 	m := map[int]string{
-		1: "victory",
+		1: "fist",
+		2: "second",
+		3: "third",
 	}
 
-	fmt.Println(m)
-
+	v, ok := m[3]
+	fmt.Println(v, "это зероноль")
+	fmt.Println(ok, "это true and false")
 }
