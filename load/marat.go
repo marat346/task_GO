f549b12d330e6eec1d93168d4abca6083e8b2e62
marat346 <dela346@mail.ru>
2023-03-19 10:59:47 +0500

    edit

--- load/marat.go
+++ load/marat.go
@@ -1,25 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"os"
-)
+import "fmt"
 
 func main() {
-	f, err := os.Open("fod.txt")
-	if err != nil {
-		fmt.Println("fall", err)
-		return
-	}
-	defer f.Close()
-
-	buf := make([]byte, 256)
-	if _, err := io.ReadFull(f, buf); err != nil {
-		fmt.Println("fall", err)
-		return
-	}
-	defer f.Close()
-
-	fmt.Println(string((f)))
+	m := "marat"
+	fmt.Println([]byte(m))
 }
