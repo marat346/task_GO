e185dd189feefb44851511af39343888a53781fe
marat346 <89637847+marat346@users.noreply.github.com>
2023-03-23 06:18:34 +0000

    edit

--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import(
   "fmt"
-  "time"
+  "log"
+"os"
+"time"
 )
 
-func runAndWait() int{
+func runAdnWaitEasy() int{
   time.Sleep(time.Second * 1)
   return 10
 }
 
-func main() {
+
+
+
+func main(){
+  file,err := os.OpenFile("logFile.txt",os.O_CREATE | os.O_WRONLY | os.O_APPEND,0666)
+  if err != nil{
+    log.Fatalf("error while opening a file:%v\n", err)
+  }
+defer file.Close()
+
+log.SetOutput(file)
+
   for i:= 0;i < 5;i++{
-    a:= runAndWait()
-    fmt.Println(time.Now(),"runAndWait finished...", "result", a)
+    a := runAdnWaitEasy()
+    log.Println("runAndWait finished...")
+    log.Println("result", a)
   }
   fmt.Println("done")
 }
