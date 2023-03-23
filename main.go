bc24e618c520d0e2f20a8208b115b23940fdb47e
marat346 <89637847+marat346@users.noreply.github.com>
2023-03-23 04:47:44 +0000

    Initial commit

--- main.go
+++ main.go
@@ -1,44 +1,11 @@
 package main
 
 import (
-	"fmt"
-  "strconv"
-)
+  "fmt"
+"strconv"
+  "strings")
 
-func main() {
-	fmt.Println("Введите число или 'стоп' чтобы завершить программу")
-	var content string
-	for {
-		fmt.Scan(&content)
-    if content == "стоп" {
-      break
-    }
-
-    num, err := strconv.Atoi(content)
-    if err != nil {
-      fmt.Println("Ошибка:", err)
-    }
-    
-		square := square(num)
-		mult := multiplication(square)
-		fmt.Println("Произведение:", <-mult)
-	}
-}
-
-func square(num int) chan int {
-	fChan := make(chan int)
-	go func() {
-		fChan <- num * num
-	}()
-	return fChan
-}
-
-func multiplication(fChan chan int) chan int {
-	sChan := make(chan int)
-	fChanRes := <-fChan
-	fmt.Println("Квадрат:", fChanRes)
-	go func() {
-		sChan <- fChanRes * 2
-	}()
-	return sChan
+type Storage interface{
+  Add (int) bool
+  Size (int)
 }
