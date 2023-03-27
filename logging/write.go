fe34fe9968fe12a5b509215b338f32f16b74e46f
marat346 <dela346@mail.ru>
2023-03-27 21:31:58 +0500

    edit

---
+++ logging/write.go
@@ -0,0 +1,31 @@
+package write
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+func runAndWait() int {
+	time.Sleep(time.Second * 1)
+	return 10
+}
+
+func main() {
+
+	file, err := os.OpenFile("test.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error while opening a file:%v\n", err)
+	}
+
+	log.SetOutput(file)
+
+	for i := 0; i < 5; i++ {
+		a := runAndWait()
+		log.Println("runAndWait finished...")
+		log.Println("result", a)
+	}
+
+	fmt.Println("done")
+}
