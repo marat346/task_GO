79c44af8eba168f503fccdecf56b950ef08b4b0b
marat346 <dela346@mail.ru>
2023-04-08 09:34:16 +0500

    edit

--- goroutine/write.go
+++ goroutine/write.go
@@ -3,40 +3,27 @@ package main
 import "fmt"
 
 func main() {
+	oneChan := make(chan string)
 
-	fc1 := one()
-	hc1 := one1(fc1)
-	sc1 := one2(hc1)
+	go func() {
 
-	fmt.Println(<-sc1)
+		oneChan <- "marat molodec"
+	}()
+
+	a := <-oneChan
+
+	fmt.Println(a)
 
 }
 
-func one() chan string {
-	firstChan1 := make(chan string)
-	go func() {
-		firstChan1 <- "складываюю книги"
-	}()
+func one() {
 
-	return firstChan1
 }
 
-func one1(firstChan1 chan string) chan string {
-	secondChan1 := make(chan string)
-	fmt.Println(<-firstChan1)
-	go func() {
-		secondChan1 <- "доставляю книги"
-	}()
+func one1() {
 
-	return firstChan1
 }
 
-func one2(secondChan1 chan string) chan string {
-	thirdChan1 := make(chan string)
-	fmt.Println(<-secondChan1)
-	go func() {
-		thirdChan1 <- "сжигаю книги"
-	}()
+func one2() {
 
-	return thirdChan1
 }
