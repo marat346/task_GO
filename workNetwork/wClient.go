5be4a5e71127369c511786cb65d3d9381b322f23
marat346 <dela346@mail.ru>
2023-04-11 09:28:24 +0500

    edit

--- workNetwork/wClient.go
+++ workNetwork/wClient.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"log"
-	"net"
-	"os"
-)
-
 func main() {
-
-	d1, err := net.Dial("tcp4", "localhost:9090")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	text, _, err := bufio.NewReader(os.Stdout).ReadLine()
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	if _, err := d1.Write([]byte(text)); err != nil {
-		log.Fatalln(err)
-	}
-
-	upperText := []byte{}
-
-	if _, err := d1.Read(upperText); err != nil {
-		log.Fatalln(err)
-	}
 
 }
