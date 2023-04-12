d010792c84c1622e62c2256fb50891f0cc810515
marat346 <dela346@mail.ru>
2023-04-12 08:37:04 +0500

    edit

--- workNetwork/wHost.go
+++ workNetwork/wHost.go
@@ -1,43 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"net"
-	"strings"
-)
-
 func main() {
-
-	lis1, err := net.Listen("tcp4", "localhost:8181")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println("server is running")
-
-	con, err := lis1.Accept()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	for {
-		line1, err := bufio.NewReader(con).ReadString('\n')
-
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		fmt.Println("line:", string(line1))
-
-		upperLine1 := strings.ToUpper(line1)
-
-		if _, err := con.Write([]byte(upperLine1)); err != nil {
-			if err != nil {
-				log.Fatalln(err)
-			}
-		}
-
-	}
 
 }
