b7ccc82a5d07e4ff1318cd1bb004c35689d85152
marat346 <dela346@mail.ru>
2023-04-13 09:42:52 +0500

    edit

--- workNetwork/JSON.go
+++ workNetwork/JSON.go
@@ -1,41 +1,67 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
-type service struct {
-	store map[string]string
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age`
+}
+
+// Создаем метод toString
+
+func (u *User) toString() string {
+	return fmt.Sprintf("name is %s and age is %d \n", u.Name, u.Age)
+}
+
+type serviceNew struct {
+	store map[string]*User
 }
 
 func main() {
 
 	mux := http.NewServeMux()
-	srv := service{make(map[string]string)}
+	srv := serviceNew{make(map[string]*User)}
 
 	mux.HandleFunc("/create", srv.Create)
 	mux.HandleFunc("/get", srv.GetAll)
 
+	fmt.Println("server is running")
 	http.ListenAndServe("localhost:8080", mux)
 }
 
 // Метод Запрос имеет POST
-func (s *service) Create(w http.ResponseWriter, r *http.Request) {
+func (s *serviceNew) Create(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		content, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
+			return
 		}
 
-		splittedContent := strings.Split(string(content), " ")
-		s.store[splittedContent[0]] = string(content)
+		var u *User
+
+		if err := json.Unmarshal(content, u); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Убрали отсюда строку
+		//  splittedContent := strings.Split(string(content), " ")
+
+		//Изменил строку
+		// s.store[splittedContent[0]] = string(content)
+
+		s.store[u.Name] = u
 
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("User was created" + splittedContent[0]))
-		return
+		w.Write([]byte("User was created" + u.Name))
 	}
 
 	w.WriteHeader(http.StatusBadRequest)
@@ -43,11 +69,12 @@ func (s *service) Create(w http.ResponseWriter, r *http.Request) {
 
 // Метод GET который возращает всех пользователей
 
-func (s *service) GetAll(w http.ResponseWriter, r *http.Request) {
+func (s *serviceNew) GetAll(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		response := " "
 		for _, user := range s.store {
-			response += user + "\n"
+			// Вызываем toString()
+			response += user.toString()
 		}
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(response))
