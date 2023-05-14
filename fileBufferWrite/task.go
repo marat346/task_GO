1abcf6ee0e8971da2b3309aee937ba22e22fcb8b
marat346 <dela346@mail.ru>
2023-05-14 13:58:55 +0500

    edit

--- fileBufferWrite/task.go
+++ fileBufferWrite/task.go
@@ -1,15 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
-func main() {
+func createFile() {
+	text := "Rune literal is expressed as one or more characters in single quotes, excluding unquoted single quotes and newlines.String literal is a concatenation of characters, a character sequence.That show st the darkness thou canst not dispel."
+
 	file, err := os.Create("marat.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	file.WriteString("privet marat")
+	defer file.Close()
+	file.WriteString(text)
+}
+
+func readFull() {
+	f, err := os.Open("marat.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	buf := make([]byte, 128)
+
+	if _, err := io.ReadFull(f, buf); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", buf)
+}
+
+func read() {
+	f, err := os.Open("marat.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	buf := make([]byte, 128)
+	_, err = f.Read(buf)
+	log.Fatal(err)
+
+	fmt.Println(string(buf))
+}
+
+func main() {
+	createFile()
+	//readFull()
+	read()
+
 }
