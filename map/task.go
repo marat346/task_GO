63d967310204c6facb47ea19e40dd6b91e013f62
marat346 <dela346@mail.ru>
2023-05-21 22:13:57 +0500

    edit

--- map/task.go
+++ map/task.go
@@ -3,15 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	mass := [2]int{5, 5}
-	fmt.Println(mass)
-
-	slice := []int{}
-	fmt.Println(slice)
-
-	m := map[int]int{
-		1: 1,
-	}
-
+	m := make(map[int]int)
+	m[1] = 5
 	fmt.Println(m)
 }
